f5ef7889cf551c008159f47e7f94c80d6dc77780
lukanthrope <bydlocoder228@gmail.com>
2020-06-13 16:18:08 +0300

    fulfill cell with header input

--- src/components/FormulaInput/index.tsx
+++ src/components/FormulaInput/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './style.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { State } from '../../reducers';
+import { updateCell } from '../../actions/table';
 
 function FormulaInput() {
+  const getActiveCell = (state: State) => state.get('activeCell');
+  const activeCell = useSelector(getActiveCell);
+  const getCellValue = (state: State) => state.get('table')[activeCell];
+  const cellValue = useSelector(getCellValue);
+  const dispatch = useDispatch();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(updateCell({
+      cell: activeCell,
+      data: e.target.value,
+    }));
+  };
+
   return (
     <header>
       <span>fx</span>
-      <input />
+      <input value={cellValue} onChange={handleChange} />
     </header>
   )
 }
