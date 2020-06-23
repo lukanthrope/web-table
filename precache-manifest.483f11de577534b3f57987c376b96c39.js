39b5630bf34a10677f6138d77e6af680656d8201
lukanthrope <bydlocoder228@gmail.com>
2020-06-23 17:06:13 +0300

    Updates

---
+++ precache-manifest.483f11de577534b3f57987c376b96c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d3a64f707db652aabef12a5f5314ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "171489241e2b608c2a20",
+    "url": "./static/css/main.34bdd62c.chunk.css"
+  },
+  {
+    "revision": "0a19385aefc7584266f2",
+    "url": "./static/js/2.ba1c2c3a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.ba1c2c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171489241e2b608c2a20",
+    "url": "./static/js/main.f75905a6.chunk.js"
+  },
+  {
+    "revision": "758c18a06de032dbc56c",
+    "url": "./static/js/runtime-main.3d0c00ff.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//lukanthrope.github.io/precache-manifest.490ba2eca93437b8c91853b8261a2c08.js"
+  "./precache-manifest.483f11de577534b3f57987c376b96c39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//lukanthrope.github.io/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
